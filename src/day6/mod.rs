d37227dfda00eafb0e3846466caa0cbe77a52b98
ingo Junefjord <lord.ingo@gmail.com>
2023-12-06 08:30:49 +0100

    Day 6

---
+++ src/day6/mod.rs
@@ -0,0 +1,31 @@
+use std::iter::zip;
+
+
+pub fn run(input: String) -> (String, String) {
+    let mut prod = 1;
+    let (line1, line2) = input.split_once('\n').unwrap();
+    let mut p2time = String::new();
+    let mut p2dist = String::new();
+    for (time, distance) in zip(line1.split_ascii_whitespace().skip(1), line2.split_ascii_whitespace().skip(1)) {
+        p2time += time;
+        p2dist += distance;
+        let time: u32 = time.parse().unwrap();
+        let distance: u32 = distance.parse().unwrap();
+        let mut wins = 0;
+        for speed in 0..time {
+            if speed * (time - speed) > distance {
+                wins += 1;
+            }
+        }
+        prod *= wins;
+    }
+    let p2time: u64 = p2time.parse().unwrap();
+    let p2dist: u64 = p2dist.parse().unwrap();
+    let mut wins = 0;
+    for speed in 0..p2time {
+        if speed * (p2time - speed) > p2dist {
+            wins += 1;
+        }
+    }
+    (format!("{prod}"),format!("{wins}"))
+}
