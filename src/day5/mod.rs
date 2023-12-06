546df1e203cb6909f0fc78a8ee9d4043fbf1a108
ingo Junefjord <ingo.junefjord@attentec.se>
2023-12-06 14:18:24 +0100

    Slight improvement to day five. 99% less memory, 70% less runtime

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,22 +1,25 @@
+use std::cmp::min;
+
 use rayon::prelude::*;
 #[derive(Debug)]
 struct Mapping {
     dest: u64,
     source: u64,
-    len: u64
+    len: u64,
 }
 
 pub fn run(input: String) -> (String, String) {
     let mut lines = input.lines();
     let seeds = lines.next().unwrap();
-    let mut seeds: Vec<u64> = seeds.strip_prefix("seeds: ").unwrap().split(' ').map(|x| x.parse().unwrap()).collect();
-    let mut seeds2 = vec![];
-    let seed_pairs = seeds.chunks(2);
-    for slice in seed_pairs {
-        for seed in slice[0]..slice[0]+slice[1] {
-            seeds2.push(seed);
-        }
-    }
+    let mut seeds: Vec<u64> = seeds
+        .strip_prefix("seeds: ")
+        .unwrap()
+        .split(' ')
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let seeds2 = seeds.clone();
+    let seed_pairs = seeds2.chunks(2);
+
     let mut mappings: Vec<Vec<Mapping>> = vec![];
     let mut cur_mapping = vec![];
     lines.next();
@@ -37,10 +40,10 @@ pub fn run(input: String) -> (String, String) {
         cur_mapping.push(Mapping {
             dest: split.next().unwrap().parse().unwrap(),
             source: split.next().unwrap().parse().unwrap(),
-            len: split.next().unwrap().parse().unwrap()
+            len: split.next().unwrap().parse().unwrap(),
         });
     }
-    
+
     mappings.push(cur_mapping);
 
     for seed in seeds.iter_mut() {
@@ -53,15 +56,29 @@ pub fn run(input: String) -> (String, String) {
             }
         }
     }
-    seeds2.par_iter_mut().for_each(|seed| {
-        for map_type in &mappings {
-            for map in map_type {
-                if *seed >= map.source && *seed < (map.source + map.len) {
-                    *seed = map.dest + (*seed - map.source);
-                    break;
-                }
-            }
-        }
-    });
-    (format!("{}", seeds.iter().min().unwrap()), format!("{}", seeds2.iter().min().unwrap()))
+    let mut min_seed = u64::MAX;
+    for slice in seed_pairs {
+        min_seed = min(
+            min_seed,
+            (slice[0]..slice[0] + slice[1])
+                .into_par_iter()
+                .map(|mut seed| {
+                    for map_type in &mappings {
+                        for map in map_type {
+                            if seed >= map.source && seed < (map.source + map.len) {
+                                seed = map.dest + (seed - map.source);
+                                break;
+                            }
+                        }
+                    }
+                    seed
+                })
+                .min()
+                .unwrap(),
+        );
+    }
+    (
+        format!("{}", seeds.iter().min().unwrap()),
+        format!("{}", min_seed),
+    )
 }
